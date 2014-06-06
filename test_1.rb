03fadbf0def5711ded93ec009ebe39e2fc6e3427
nihal-qburst <nihal@qburst.com>
2014-06-06 13:54:52 +0530

    Delete test_1.rb

--- test_1.rb
+++
@@ -1,9 +0,0 @@
-require 'rexml/document'
-require 'open-uri'
-include REXML
-
-uri = URI.parse("http://www.qburst.com/sitemap.xml")
-xmldoc  = Document.new (uri.open)
-root = xmldoc.root
-print root,"\n"
-
