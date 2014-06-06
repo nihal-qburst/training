159ffe90fb91de5a600e2537495a74fe3774041e
Nihal Nizar <nihal@qburst.com>
2014-06-06 11:04:56 +0530

    parse and store xml to MySQL

--- one.rb
+++ one.rb
@@ -1,13 +1,21 @@
-require 'rexml/document'
-require 'open-uri'
-include REXML
+require 'mysql2'
+begin
+ 	db = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "qburst")
+	#db.query("CREATE DATABASE test")
+	db.select_db('qb_sitemap_task1')
+	#db.query("CREATE TABLE users (name varchar(20),age integer)")
+	#db.query("INSERT INTO users VALUES('nicku',22)")
+	results = db.query("SELECT * FROM sitemap")
+	
+	results.each do |row|
+    	puts row
+    end
 
-uri = URI.parse("http://www.qburst.com/sitemap.xml")
-uri.open {|f|
-	xmldoc = Document.new(f)
-	xmldoc.elements.each("urlset/url/loc"){ 
-		|e| puts "Location : " + e.text
-	}
-}
 
-
+rescue Mysql2::Error => e
+  puts "Error code: #{e.errno}"
+  puts "Error message: #{e.error}"
+  puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
+ensure
+  db.close if db
+end
